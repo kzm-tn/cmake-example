b65d52be5ecda67a4de8fad70235c4a86c3705fa
Tani_macbookpro2016 <tani@aero.t.u-tokyo.ac.jp>
2020-07-02 08:40:01 +0900

    separate problem and answer

--- cmake-hybrid-example/src/main.f90
+++ cmake-hybrid-example/src/main.f90
@@ -1,17 +1,4 @@
 program main
-!$ use omp_lib
       use c
-      implicit none
-      include "mpif.h"
-      integer :: nprocs
-      integer :: myrank
-      integer :: ierr
-      call mpi_init(ierr)
-      call mpi_comm_size(MPI_COMM_WORLD, nprocs, ierr)
-      call mpi_comm_rank(MPI_COMM_WORLD, myrank, ierr)
-      print *, "I am", myrank, "/", nprocs
-!$omp parallel
-!$    print *, "Hello! N=", omp_get_num_threads(), " and I am", omp_get_thread_num(), " / ",myrank
-!$omp end parallel
-      call mpi_finalize(ierr)
+      print *, "hello world!"
 end program main
--- cmake-mpi-example/src/main.f90
+++ cmake-mpi-example/src/main.f90
@@ -1,13 +1,4 @@
 program main
       use c
-      implicit none
-      include "mpif.h"
-      integer :: nprocs
-      integer :: myrank
-      integer :: ierr
-      call mpi_init(ierr)
-      call mpi_comm_size(MPI_COMM_WORLD, nprocs, ierr)
-      call mpi_comm_rank(MPI_COMM_WORLD, myrank, ierr)
-      print *, "I am", myrank, "/", nprocs
-      call mpi_finalize(ierr)
+      print *, "hello world!"
 end program main
